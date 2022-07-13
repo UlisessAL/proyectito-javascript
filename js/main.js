88c4e2a53af95ecf37221ef36c960a02bdaddc8b
¨Ulises¨ <larrabeulises@gmail.com>
2022-07-13 18:57:10 -0300

    Mejoras en la página

--- js/main.js
+++ js/main.js
@@ -2,6 +2,9 @@ const botonIniciar = document.querySelector(".boton-iniciar"),
 sectionInicio = document.querySelector(".inicio"),
 formulario = document.querySelector(".formulario");
 
+const botonMenu = document.querySelector(".boton-menu"),
+cartas = document.querySelector(".cartas");
+
 
 
 botonIniciar.addEventListener("click", ()=> {
@@ -60,6 +63,69 @@ botonIniciar.addEventListener("click", ()=> {
             guardar("sessionStorage");
         }
 
+        const formularioDos = document.querySelector("#formulario-2")
+
+        formularioDos.id = "mostrar-boton-menu"
+
+
     })
 
 })
+
+
+botonMenu.addEventListener("click", ()=> {
+    cartas.id = "mostrar-cartas"
+})
+
+const menu =[]
+
+function Plato(id, nombre, precio) {
+    this.id = id;
+    this.nombre = nombre;
+    this.precio = precio;
+}
+
+function crearPlato(id,nombre,precio){
+    const plato = new Plato(id,nombre,precio);
+    return plato;
+}
+
+function cargarPlato(plato){
+    menu.push(plato);
+}
+
+const cheesecake = crearPlato(1,"Cheesecake de Frambuesas", 2000),
+donaChocolate = crearPlato(2,"Dona bañada en chocolate negro", 250),
+waffles = crearPlato(3, "Waffles con Frutilla y arándanos", 2300);
+
+cargarPlato(cheesecake)
+cargarPlato(donaChocolate);
+cargarPlato(waffles);
+
+
+const carrito = [];
+
+
+function agregarCarrito(arr, item) {
+    return arr.push(item);
+}
+
+const botonCheesecake = document.querySelector("#añadir-carrito-cheesecake"),
+botonDona = document.querySelector("#añadir-carrito-dona"),
+botonWaffles = document.querySelector("#añadir-carrito-waffles");
+
+botonCheesecake.addEventListener("click", ()=> {
+    agregarCarrito(carrito, cheesecake);
+    console.log(carrito);
+})
+
+botonDona.addEventListener("click", ()=> {
+    agregarCarrito(carrito, donaChocolate);
+    console.log(carrito);
+})
+
+botonWaffles.addEventListener("click", ()=> {
+    agregarCarrito(carrito, waffles);
+    console.log(carrito);
+})
+
